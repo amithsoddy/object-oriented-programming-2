c1708c3c4798252f848be40f97d3453ecd906061
amithsoddy <anithasoddy@gmail.com>
2024-10-19 18:40:33 +0530

    constructor and destructor

---
+++ lms.py
@@ -0,0 +1,11 @@
+class showroom :
+    def __init__(self,list_of_cars,name):
+        self.booklist=list_of_cars
+        self.lendict={}
+        def displaycars(self):
+            print(f "we have the following cars in our showroom :{self.name}")
+            for car in self.carslist:
+                print(car)
+                def lendcar(self,user,cars):
+                    
+
